19087f8f45560e3fad3ffa1d75ab4bf1e7605802
iheese <imbc0224@gmail.com>
2023-07-12 15:47:20 +0900

    정답 확인용 출력 메소드 생성

--- src/test/java/com/mangkyu/stream/Quiz1/Quiz1Test.java
+++ src/test/java/com/mangkyu/stream/Quiz1/Quiz1Test.java
@@ -5,19 +5,22 @@ import java.io.IOException;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-class Quiz1Test {
+class Quiz1Test<T> {
 
     private final Answer1 answer = new Answer1();
     private final Quiz1 quiz = new Quiz1();
+    private T t;
 
     @Test
     void quiz1() throws IOException {
         assertThat(quiz.quiz1()).isEqualTo(answer.quiz1());
+        print((T) answer.quiz1(), (T) quiz.quiz1()); // 정답과 나의 답을 출력한다.
     }
 
     @Test
     void quiz2() throws IOException {
         assertThat(quiz.quiz2()).isEqualTo(answer.quiz2());
+        print((T) answer.quiz2(), (T) quiz.quiz2());
     }
 
     @Test
@@ -25,4 +28,8 @@ class Quiz1Test {
         assertThat(quiz.quiz3()).isEqualTo(answer.quiz3());
     }
 
+    private void print(T answer, T quiz) {
+        System.out.println("문제 정답 출력 결과 : " + answer);
+        System.out.println("나의 정답 출력 결과 : " + quiz);
+    }
 }
