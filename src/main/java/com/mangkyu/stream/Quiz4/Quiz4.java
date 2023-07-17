4d26ebd307e7235f846a496373abd5e9e838f93a
iheese <imbc0224@gmail.com>
2023-07-17 17:28:59 +0900

    퀴즈4 문제 풀이

--- src/main/java/com/mangkyu/stream/Quiz4/Quiz4.java
+++ src/main/java/com/mangkyu/stream/Quiz4/Quiz4.java
@@ -2,8 +2,9 @@ package com.mangkyu.stream.Quiz4;
 
 
 import java.util.Arrays;
-import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Quiz4 {
 
@@ -27,37 +28,78 @@ public class Quiz4 {
 
     // 4.1 2020년에 일어난 모든 거래 내역을 찾아 거래값을 기준으로 오름차순 정렬하라.
     public List<Transaction> quiz1() {
-        return Collections.emptyList();
+        return transactions.stream()
+                .filter(transaction -> transaction.getYear() == 2020) // 2020년도 조건
+                .sorted(Comparator.comparing(Transaction::getValue)) // sorted Transaction 거래값 비교, 내림차순은 reversed() 뒤에 붙이기
+                .collect(Collectors.toList()); // 리스트화
     }
 
     // 4.2 거래 내역이 있는 거래자가 근무하는 모든 도시를 중복 없이 나열하라.
     public List<String> quiz2() {
-        return Collections.emptyList();
+        return transactions.stream()
+                .map(transaction -> transaction.getTrader().getCity()) // 거래 내역의 거래자의 도시로 변환
+                .distinct() // 중복 없이 처리
+                .collect(Collectors.toList()); // 리스트화
     }
 
     // 4.3 서울에서 근무하는 모든 거래자를 찾아서 이름순서대로 정렬하라.
     public List<Trader> quiz3() {
-        return Collections.emptyList();
+        return transactions.stream()
+                .map(transaction -> transaction.getTrader()) // 거래내역의 거래자
+                .distinct() // 중복이면 제거
+                .filter(trader -> trader.getCity().equals("Seoul")) // 서울에서 거래했다면
+                .sorted(Comparator.comparing(Trader::getName)) // 거래자 이름 순서대로(오름차순)
+                .collect(Collectors.toList()); // 리스트화
     }
 
     // 4.4 모든 거래자의 이름을 구분자(",")로 구분하여 정렬하라.
     public String quiz4() {
-        return null;
+        return transactions.stream()
+                .map(transaction -> transaction.getTrader().getName()) // 거래 내역의 거래자 이름 추출
+                .distinct() // 중복 제거
+                .sorted() //  String 스트림 오름차순 정렬
+                .collect(Collectors.joining(",")); // 구분자 포함해서 String 값으로 만들기
     }
 
     // 4.5 부산에 거래자가 있는지를 확인하라.
     public boolean quiz5() {
-        return false;
+        return transactions.stream()
+                // 거래자의 도시에 Busan 이 있는지 확인
+                .anyMatch(transaction -> transaction.getTrader().getCity().equals("Busan"));
     }
 
     // 4.6 서울에 거주하는 거래자의 모든 거래 금액을 구하라.
     public List<Integer> quiz6() {
-        return Collections.emptyList();
+        return transactions.stream()
+                .filter(transaction -> transaction.getTrader().getCity().equals("Seoul")) // 거래 내역의 거래자의 도시가 Seoul 인지 확인
+                .map(Transaction::getValue) // 일치하면 해당 거래 내역의 거래 금액 변경
+                .collect(Collectors.toList()); // 모든 거래 금액 리스트화
     }
 
     // 4.7 모든 거래 내역중에서 거래 금액의 최댓값과 최솟값을 구하라. 단, 최댓값은 reduce를 이용하고 최솟값은 stream의 min()을 이용하라.
     public Integer[] quiz7() {
-        return new Integer[]{0, 0};
-    }
+        Integer[] array = new Integer[2];
+        array[0] = transactions.stream()
+                .map(Transaction::getValue) // 거래 내역의 거래 금액을 가져옵니다.
+                // reduce는 스트림 여러 개의 값을 하나로 만듭니다.
+                .reduce(Integer::max) // 거래 금액 중 최대 값을 가져옵니다.
+                .orElse(-1); // 아니면 -1 을 출력합니다.
 
+//        array[0] = transactions.stream()
+//                .max(Comparator.comparingInt(Transaction::getValue))
+//                .orElseThrow(RuntimeException::new).getValue();
+
+        array[1] = transactions.stream()
+                .min(Comparator.comparingInt(Transaction::getValue)) // 거래 내역 스트림에서 거래 금액을 비교해 최솟값을 가져옵니다.
+                .orElseThrow(RuntimeException::new).getValue(); // 존재하지 않으면 예외를 던진다.
+
+//        array[1] = transactions.stream()
+//                .map(Transaction::getValue) // 거래 내역의 거래 금액을 가져옵니다.
+//                // reduce는 스트림 여러 개의 값을 하나로 만듭니다.
+//                .reduce(Integer::min) // 거래 금액 중 최대 값을 가져옵니다.
+//                .orElse(-1); // 아니면 -1 을 출력합니다.
+
+        // 최솟값과 최댓값을 구하는 건 두 가지 방식으로 가능하다.
+        return array;
+    }
 }
